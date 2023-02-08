07423ef48805f8c2dd695e4bd70a419aeacecaf5
Benjamin E. Iserman <winggar1228@gmail.com>
2023-02-08 11:18:15 -0600

    add JENNY constant

--- WingCalcShell/Properties/VersionInfo.cs
+++ WingCalcShell/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-[assembly: System.Reflection.AssemblyFileVersion("0.1.315.0")]
+[assembly: System.Reflection.AssemblyFileVersion("0.1.372.0")]
 [assembly: System.Reflection.AssemblyVersion("0.1.0.0")]
